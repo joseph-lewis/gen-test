070f404817da24f82fdc8e644271b85aaa37161f
joe@zutapp.com <joe@zutapp.com>
2026-02-22 15:05:16 +1000

    feat: Add index page to fix 404 error

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to your Next.js App!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
